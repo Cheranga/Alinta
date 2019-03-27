2cfad070a18fdf6bc82a17f735fb978efc3e5eac
Cheranga Hatangala <cheranga@gmail.com>
2019-03-28 04:24:44 +1100

    Adding the delete and update related integration tests and, updated the project dependency graph with the new integration test project.

--- Alinta.WebApi.IntegrationTests/CustomersControllerTests.cs
+++ Alinta.WebApi.IntegrationTests/CustomersControllerTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Alinta.WebApi.DTO.Requests;
 using Alinta.WebApi.Models;
@@ -12,7 +13,7 @@ using Xunit;
 
 namespace Alinta.WebApi.IntegrationTests
 {
-    public class CustomersControllerTests: IClassFixture<WebApplicationFactory<Startup>>
+    public class CustomersControllerTests: IClassFixture<WebApplicationFactory<Startup>>, IDisposable
     {
         private const string BaseUrl = @"/api/v1/customers";
         private readonly HttpClient _client;
@@ -70,21 +71,22 @@ namespace Alinta.WebApi.IntegrationTests
         }
 
         [Fact]
-        public async Task When_There_are_Customers_Which_Matches_The_Name_Filter_They_Will_Be_Returned()
+        public async Task When_There_Are_Customers_Which_Matches_The_Name_Filter_They_Will_Be_Returned()
         {
             //
             // Arrange
             //
+            var commonName = Guid.NewGuid().ToString();
             var johnSnow = new CreateCustomerRequest
             {
-                FirstName = "john",
+                FirstName = commonName,
                 LastName = "snow",
                 DateOfBirth = new DateTime(1982, 1, 1)
             };
             var littleJohn = new CreateCustomerRequest
             {
                 FirstName = "little",
-                LastName = "john",
+                LastName = commonName,
                 DateOfBirth = new DateTime(1982, 1, 1)
             };
             await _client.PostAsJsonAsync(BaseUrl, johnSnow);
@@ -92,7 +94,7 @@ namespace Alinta.WebApi.IntegrationTests
             //
             // Act
             //
-            var httpResponse = await _client.GetAsync($"{BaseUrl}?name=john");
+            var httpResponse = await _client.GetAsync($"{BaseUrl}?name={commonName}");
             //
             // Assert
             //
@@ -121,11 +123,176 @@ namespace Alinta.WebApi.IntegrationTests
             //
             // Act
             //
-            var httpResponse = await _client.GetAsync($"{BaseUrl}?name=john");
+            var httpResponse = await _client.GetAsync($"{BaseUrl}?name={Guid.NewGuid().ToString()}");
             //
             // Assert
             //
            Assert.Equal(HttpStatusCode.NotFound, httpResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task Invalid_Edit_Customer_Request_Must_Fail()
+        {
+            //
+            // Arrange
+            //
+            var request = new UpdateCustomerRequest
+            {
+                Id = "",
+                FirstName = ""
+            };
+            //
+            // Act
+            //
+            var httpResponse = await _client.PutAsync("", null);
+            //
+            // Assert
+            //
+            Assert.False(httpResponse.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Customers_Who_Are_Not_Existing_Cannot_Be_Updated()
+        {
+            //
+            // Arrange
+            //
+            var request = new UpdateCustomerRequest
+            {
+                Id = "",
+                FirstName = ""
+            };
+            //
+            // Act
+            //
+            var httpResponse = await _client.PutAsync("", null);
+            //
+            // Assert
+            //
+            Assert.False(httpResponse.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task With_Valid_Update_Customer_Request_Existing_Customer_Can_Be_Updated()
+        {
+            //
+            // Arrange
+            //
+            var johnSnow = new CreateCustomerRequest
+            {
+                FirstName = "john",
+                LastName = "snow",
+                DateOfBirth = new DateTime(2000, 1, 1)
+            };
+            var httpResponse = await _client.PostAsJsonAsync(@"/api/v1/customers", johnSnow);
+            var createdCustomerDto = JsonConvert.DeserializeObject<DisplayCustomerDto>(await httpResponse.Content.ReadAsStringAsync());
+            var id = createdCustomerDto.Id;
+
+            var updateRequest = new UpdateCustomerRequest
+            {
+                Id = id,
+                FirstName = "John",
+                LastName = "Connor",
+                DateOfBirth = new DateTime(1990, 1, 1)
+            };  
+            var httpRequest = new HttpRequestMessage(HttpMethod.Put, BaseUrl)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(updateRequest), Encoding.UTF8, "application/json")
+            };
+            //
+            // Act
+            //
+            httpResponse = await _client.SendAsync(httpRequest);
+            //
+            // Assert
+            //
+            httpResponse.EnsureSuccessStatusCode();
+            var updatedCustomerDto = JsonConvert.DeserializeObject<DisplayCustomerDto>(await httpResponse.Content.ReadAsStringAsync());
+
+            Assert.NotNull(updatedCustomerDto);
+            Assert.Equal(id, updatedCustomerDto.Id);
+            Assert.Equal("John Connor", updatedCustomerDto.Name);
+        }
+
+        [Fact]
+        public async Task Invalid_Delete_Customer_Request_Must_Fail()
+        {
+            //
+            // Arrange
+            //
+            var request = new DeleteCustomerRequest
+            {
+                CustomerId = ""
+            };
+            //
+            // Act
+            //
+            var httpResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Delete, BaseUrl)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json")
+            });
+            //
+            // Assert
+            //
+            Assert.False(httpResponse.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task Customers_Who_Are_Not_Existing_Cannot_Be_Deleted()
+        {
+            //
+            // Arrange
+            //
+            var request = new DeleteCustomerRequest
+            {
+                CustomerId = Guid.NewGuid().ToString()
+            };
+            //
+            // Act
+            //
+            var httpResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Delete, BaseUrl)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json")
+            });
+            //
+            // Assert
+            //
+            Assert.False(httpResponse.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task With_Valid_Request_Existing_Customer_Can_Be_Deleted()
+        {
+            //
+            // Arrange
+            //
+            var httpResponse = await _client.PostAsJsonAsync(BaseUrl, new CreateCustomerRequest
+            {
+                FirstName = "john",
+                LastName = "snow",
+                DateOfBirth = new DateTime(2000, 1, 1)
+            });
+            var createdCustomerDto = JsonConvert.DeserializeObject<DisplayCustomerDto>(await httpResponse.Content.ReadAsStringAsync());
+            var deleteRequest = new DeleteCustomerRequest
+            {
+                CustomerId = createdCustomerDto.Id
+            };
+            //
+            // Act
+            //
+            httpResponse = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Delete, BaseUrl)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(deleteRequest), Encoding.UTF8, "application/json")
+            });
+            //
+            // Assert
+            //
+            httpResponse.EnsureSuccessStatusCode();
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
         }
     }
 }
